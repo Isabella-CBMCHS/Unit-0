956b4676473662bc5e6968294da79acd662a97c1
Isabella Corser <itcorser25@students.bmchs.org>
2022-08-18 08:14:27 -0500

    added a lab file

---
+++ Lab0.01.py
@@ -0,0 +1,66 @@
+'''
+###############
+# Do Now 0.01 #
+###############
+
+Read through the following code:
+--------------------------------
+my_pet_1 = 'pet'
+my_pet_1_type = 'cat'
+my_pet_1_noise = 'meow'
+my_pet_1_full_name = 'Snuffles McGruff'
+
+my_pet_2 = 'pet'
+my_pet_2_type = 'cat'
+my_pet_2_noise = 'meow'
+my_pet_2_full_name = 'Snowpounce Flury'
+
+my_pet_3 = 'pet'
+my_pet_3_type = 'cat'
+my_pet_3_noise = 'meow'
+my_pet_3_full_name = 'Snickers Snorkel'
+
+my_pets = [my_pet_1, my_pet_2, my_pet_3]
+for pet in my_pets:
+    ## print full name of each pet
+
+In your Notebook
+----------------
+Respond to the following:
+
+    1.  Write a quick description of how you would print out each of the pet's names.
+
+    2.  Write down some other data structures you could use to make this easier.
+
+'''
+my_pet_1 = 'pet'
+my_pet_1_type = 'cat'
+my_pet_1_noise = 'meow'
+my_pet_1_full_name = 'Snuffles McGruff'
+
+my_pet_2 = 'pet'
+my_pet_2_type = 'cat'
+my_pet_2_noise = 'meow'
+my_pet_2_full_name = 'Snowpounce Flury'
+
+my_pet_3 = 'pet'
+my_pet_3_type = 'cat'
+my_pet_3_noise = 'meow'
+my_pet_3_full_name = 'Snickers Snorkel'
+
+class Cat():
+    def __init__(self, name):
+        self.attribute = 'pet'
+        self.species = 'cat'
+        self.noise = 'meow'
+        self.name = name
+
+cat1 = Cat('Snuffles McGruff')
+cat2 = Cat('Snowpounce Flury')
+cat3 = Cat('Snickers Snorkel')
+
+my_pets = (cat1, cat2, cat3)
+
+for pet in my_pets:
+    print(pet.name)
+    
